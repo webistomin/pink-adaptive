46d48d3714674c5d74b6eb64c7da1e547d315d4c
Alexey Istomin <webistomin@gmail.com>
2017-07-29 18:43:45 +0300

    Add files via upload
    Убрал скрипты в папку

---
+++ js/toggler.js
@@ -0,0 +1,12 @@
+var navMain = document.querySelector('.main-navigation');
+var navToggle = document.querySelector('.main-navigation__toogler');
+navMain.classList.remove('main-navigation--nojs');
+navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-navigation--closed')) {
+        navMain.classList.remove('main-navigation--closed');
+        navMain.classList.add('main-navigation--opened');
+    } else {
+        navMain.classList.add('main-navigation--closed');
+        navMain.classList.remove('main-navigation--opened');
+    }
+});
